6272c5517550f834ad5118d99426eac858b96019
Abir-Al-Arafat <antorabir@gmail.com>
2023-05-04 02:37:43 +0600

    new packages and props added
    new packages for generating id and animation added

--- Feedback Project/feedback-app/src/components/FeedbackList.jsx
+++ Feedback Project/feedback-app/src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import {motion, AnimatePresence} from 'framer-motion'
 import PropTypes from 'prop-types'
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
@@ -6,19 +7,38 @@ function FeedbackList({feedback, handleDelete}) {
   if(!feedback || feedback.length === 0){
     return <p>No Feedback yet</p>
   }
-  
-  return <div className='feedback-list'>
-    {feedback.map((item) => 
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-    )}
+
+  // with animation effects
+  return (
+  <div className='feedback-list'>
+    <AnimatePresence>
+      {feedback.map((item) => (
+          <motion.div 
+          key={item.id} 
+          initial={{opacity: 0}}
+          animate={{opacity: 1}}
+          exit={{opacity: 0}}
+          >
+            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+          </motion.div>
+      ))}
+    </AnimatePresence>
   </div>
+)
+  
+  // without animation effects
+  // return <div className='feedback-list'>
+  //   {feedback.map((item) => 
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+  //   )}
+  // </div>
 }
 
 // setting shape of object inside of the array
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.number,
       rating: PropTypes.number.isRequired,
       text: PropTypes.string
     })
